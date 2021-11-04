ad71a976d6b618dab64ef4b401933c8d150ea045
Juan Carlos Ruperto Zarate <juancarloszarate586@gmail.com>
2021-11-03 18:12:43 -0600

    Se agrego express y dotenv

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hola Juan!");
+/*console.log("Hola Juan!");*/
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (req, res) {res.send('Hola Juan'); });
+
+app.listen(3000);
